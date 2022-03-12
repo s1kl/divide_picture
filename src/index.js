eec424e9b26e1739fbe6e21699188edeec378293
s1kl <shik1ikhs@gmail.com>
2022-03-12 22:47:39 +0900

    finished divide parts

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import "./main.css";
 import Cropper from 'cropperjs';
 
 const cropperImg = document.getElementById('cropper-img');
-const cropper = new Cropper(cropperImg);
+const cropper = new Cropper(cropperImg, { aspectRatio: 16/9 });
 const canvas1 = document.getElementById("canvas1");
 const ctx1 = canvas1.getContext("2d");
 const canvas2 = document.getElementById("canvas2");
@@ -15,7 +15,9 @@ const image1 = document.getElementById("image1");
 const image2 = document.getElementById("image2");
 const image3 = document.getElementById("image3");
 const image4 = document.getElementById("image4");
-const cropBtn = document.getElementById('crop-btn')
+const cropBtn4 = document.getElementById('crop-btn4');
+const cropBtn3 = document.getElementById('crop-btn3');
+const cropBtn2 = document.getElementById('crop-btn2');
 const result = document.getElementById('result-img');
 const cropInput = document.getElementById('crop-input');
 
@@ -28,6 +30,7 @@ function cropDisplayImage(event) {
   }
   reader.readAsDataURL(file)
 }
+
 let resultImgUrl;
 function outCropped() {
   resultImgUrl = cropper.getCroppedCanvas().toDataURL();
@@ -40,6 +43,16 @@ let cropped_image_width;
 let cropped_image_height;
 const cropped_pict = new Image();
 function cropped_canvasImage() {
+  if (n == 4) {
+    combine4();
+  } else if (n ==3) {
+    combine3();
+  } else {
+    combine2();
+  }
+}
+
+function combine4() {
   cropped_image_width = result.naturalWidth / 2;
   cropped_image_height = result.naturalHeight / 2;
   canvas1.width = cropped_image_width;
@@ -62,51 +75,20 @@ function cropped_canvasImage() {
   image4.src = canvas4.toDataURL();
 }
 
-cropInput.addEventListener('change', cropDisplayImage);
-cropBtn.addEventListener('click', outCropped);
-
-
-
-
-
-
-
-
-/*
-const input = document.getElementById("input");
-const image = document.getElementById("img");
-function displayImage(event) {
-  const file = event.target.files[0]
-  if (!file) return
-  const reader = new FileReader()
-  reader.onload = (event) => {
-    image.src = event.target.result
-  }
-  reader.readAsDataURL(file)
-
-  window.setTimeout(canvasImage, 100);
-}
-
-
-let image_width;
-let image_height;
-const pict = new Image();
-function canvasImage() {
-  image_width = image.naturalWidth / 2;
-  image_height = image.naturalHeight / 2;
-  canvas1.width = image_width;
-  canvas1.height = image_height;
-  canvas2.width = image_width;
-  canvas2.height = image_height;
-  canvas3.width = image_width;
-  canvas3.height = image_height;
-  canvas4.width = image_width;
-  canvas4.height = image_height;
-  pict.src = image.src;
-  ctx1.drawImage(pict, 0, 0);
-  ctx2.drawImage(pict, -image_width, 0);
-  ctx3.drawImage(pict, 0, -image_height);
-  ctx4.drawImage(pict, -image_width, -image_height);
+function combine3() {
+  cropped_image_width = result.naturalWidth / 2;
+  cropped_image_height = result.naturalHeight / 2;
+  canvas1.width = cropped_image_width;
+  canvas1.height = result.naturalHeight;
+  canvas2.width = cropped_image_width;
+  canvas2.height = cropped_image_height;
+  canvas3.width = cropped_image_width;
+  canvas3.height = cropped_image_height;
+  cropped_pict.src = result.src;
+  ctx1.drawImage(cropped_pict, 0, 0);
+  ctx2.drawImage(cropped_pict, -cropped_image_width, 0);
+  ctx3.drawImage(cropped_pict, -cropped_image_width, -cropped_image_height);
+  ctx4.clearRect(0, 0, canvas4.width, canvas4.height);
 
   image1.src = canvas1.toDataURL();
   image2.src = canvas2.toDataURL();
@@ -114,7 +96,39 @@ function canvasImage() {
   image4.src = canvas4.toDataURL();
 }
 
+function combine2() {
+  cropped_image_width = result.naturalWidth / 2;
+  canvas1.width = cropped_image_width;
+  canvas1.height = result.naturalHeight;
+  canvas2.width = cropped_image_width;
+  canvas2.height = result.naturalHeight;
+  cropped_pict.src = result.src;
+  ctx1.drawImage(cropped_pict, 0, 0);
+  ctx2.drawImage(cropped_pict, -cropped_image_width, 0);
+  ctx3.clearRect(0, 0, canvas3.width, canvas3.height);
+  ctx4.clearRect(0, 0, canvas4.width, canvas4.height);
 
-input.addEventListener('change', displayImage);
-*/
+  image1.src = canvas1.toDataURL();
+  image2.src = canvas2.toDataURL();
+  image3.src = canvas3.toDataURL();
+  image4.src = canvas4.toDataURL();
+}
 
+let n;
+function select4() {
+  n = 4;
+  outCropped();
+}
+function select3() {
+  n = 3;
+  outCropped();
+}
+function select2() {
+  n = 2;
+  outCropped();
+}
+
+cropInput.addEventListener('change', cropDisplayImage);
+cropBtn4.addEventListener('click', select4);
+cropBtn3.addEventListener('click', select3);
+cropBtn2.addEventListener('click', select2);
